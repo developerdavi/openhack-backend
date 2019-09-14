261277fa04723d562a3fcdf3c0a659a84e6ae5cf
Davi Rodrigues <developer.davi@gmail.com>
2019-09-14 09:39:36 -0300

    Added compare method, to run the API and return match result

--- TeamMatchingAI/util.js
+++ TeamMatchingAI/util.js
@@ -1,7 +1,21 @@
 const { ExperienceAI } = require('./model')
 
+const expAI = new ExperienceAI()
+
 module.exports = {
-  compare: (p1, p2) => {
+  compare: async (p1, p2) => {
+    let allResults = [0,0,0,0]
+
+    // COMPARE EXPERIENCES
+    let input = p1.experience >= p2.experience ? p1.experience - p2.experience : p2.experience - p1.experience
     
+    let data = await expAI.run([input])
+
+    allResults[0] = data[0]
+
+    return allResults
+  },
+  train: async () => {
+    await expAI.train()
   }
 }
