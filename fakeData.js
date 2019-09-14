bf60677c432ac460a460d3f87d870c76273f5da9
Davi Rodrigues <developer.davi@gmail.com>
2019-09-14 13:49:31 -0300

    Created FakeData file

---
+++ fakeData.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    experience: 10,
+    language: ['Javascript'],
+    area: 'Back-end',
+    interest: 'Win'
+  },
+  {
+    experience: 8,
+    language: ['Java', 'C++'],
+    area: 'Front-end',
+    interest: 'Win'
+  },
+  {
+    experience: 6,
+    language: ['Javascript', 'Python'],
+    area: 'Front-end',
+    interest: 'Learn'
+  },
+  {
+    experience: 4,
+    language: ['PHP'],
+    area: 'Back-end',
+    interest: 'Learn'
+  },
+  {
+    experience: 2,
+    language: ['Python', 'Java'],
+    area: 'UI',
+    interest: 'Win'
+  },
+  {
+    experience: 0,
+    language: ['Java'],
+    area: 'UX',
+    interest: 'Learn'
+  },
+  {
+    experience: 0,
+    language: ['PHP'],
+    area: 'Back-end',
+    interest: 'Learn'
+  },
+  {
+    experience: 0,
+    language: ['Java', 'PHP'],
+    area: 'Back-end',
+    interest: 'Win'
+  }
+]
