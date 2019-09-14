ce20f8a9bf12ddc22056e1cba15e3bbc258f6343
Davi Rodrigues <developer.davi@gmail.com>
2019-09-14 13:51:37 -0300

    Changed Language verification to array and now considering the development area

--- TeamMatchingAI/util.js
+++ TeamMatchingAI/util.js
@@ -1,6 +1,7 @@
-const { ExperienceAI } = require('./model')
+const { ExperienceAI, CompareAI } = require('./model')
 
 const expAI = new ExperienceAI()
+const compareAI = new CompareAI()
 
 module.exports = {
   compare: async (p1, p2) => {
@@ -13,7 +14,15 @@ module.exports = {
 
     allResults[0] = data[0]
 
-    return allResults
+    p1.language.forEach(lang => {
+      allResults[1] += p2.language.includes(lang) ? 0.5 : 0
+    })
+
+    allResults[2] += p1.area === p2.area ? -0.5 : 1
+
+    const sum = allResults[0] + allResults [1] + allResults[2] + allResults [3]
+
+    return sum
   },
   train: async () => {
     await expAI.train()
